4376db94e05b83a211f2cfb4e846dae94ed8d5ff
Mizanur Rahaman Ashiq <mizanur35-2844@diu.edu.bd>
2022-11-14 13:10:50 +0600

    hooks started

--- 02-adopt-me-jsx/src/App.jsx
+++ 02-adopt-me-jsx/src/App.jsx
@@ -1,33 +1,14 @@
 import React from "react";
-import { createRoot } from "react-dom";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Tigresh",
-      animal: "Dog",
-      breed: "Mixed",
-    }),
-    React.createElement(Pet, {
-      name: "Doremon",
-      animal: "Cat",
-      breed: "Parsian",
-    }),
-    React.createElement(Pet, {
-      name: "Scooby",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
